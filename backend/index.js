0f2b8a25716c421f5a1ca07690904cb31603238b
Kien Ta <talekien1710@gmail.com>
2021-10-21 22:39:05 -0500

    dependencies added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ app: "instagram :) :)" });
