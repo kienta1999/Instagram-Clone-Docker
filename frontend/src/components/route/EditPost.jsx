9fa2c922a79cadf6b2318fe0e8fafbd073084f8d
Kien Ta <talekien1710@gmail.com>
2021-12-04 01:12:33 -0600

    update post functionality done

--- frontend/src/components/route/EditPost.jsx
+++ frontend/src/components/route/EditPost.jsx
@@ -1,27 +1,55 @@
 import { useParams } from "react-router";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import { Container, Row, Col } from "react-bootstrap";
 import NavigationBar from "../NavigationBar.jsx";
+import { getPost, updatePost } from "../../data/post";
 
 const EditPost = () => {
   const { getToken } = useContext(UserContext);
   const history = useHistory();
   const token = getToken();
-  const { id } = useParams();
-  console.log(token);
+  const userId = token?.id;
+  const { id: postId } = useParams();
+  const [content, setContent] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getPost(userId, postId);
+        if (result) {
+          setContent(result.data.content);
+        }
+      } catch (error) {
+        history.push("/");
+        console.log(error.response.data?.message);
+      }
+    })();
+  }, []);
   if (!token) {
     history.push("/login");
     return null;
-  } else {
-    return (
-      <Container>
-        <NavigationBar />
-        <div>Editting Post {id}</div>
-      </Container>
-    );
   }
+  const updateContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const submitUpdate = async () => {
+    await updatePost(userId, postId, content);
+    history.push("/");
+  };
+
+  return (
+    <Container>
+      <NavigationBar />
+      <div>Editting Post {postId}</div>
+      <textarea value={content} onChange={updateContent}></textarea>
+      <br />
+      <button className="btn btn-primary" onClick={submitUpdate}>
+        Update
+      </button>
+    </Container>
+  );
 };
 
 export default EditPost;
--- frontend/src/data/post.js
+++ frontend/src/data/post.js
@@ -17,8 +17,21 @@ export const listAllPost = async () => {
   return await axios.get(url);
 };
 
+export const updatePost = async (userId, postId, content) => {
+  const { backendHost } = keys;
+  const url = `${backendHost}/api/user/${userId}/post/${postId}`;
+  return await axios.put(url, { content });
+};
+
 export const deletePost = async (userId, postId) => {
   const { backendHost } = keys;
   const url = `${backendHost}/api/user/${userId}/post/${postId}`;
   return await axios.delete(url);
+};
+
+export const getPost = async (userId, postId) => {
+  if (!userId || !postId) return;
+  const { backendHost } = keys;
+  const url = `${backendHost}/api/user/${userId}/post/${postId}`;
+  return await axios.get(url);
 };
